3e85c4c72f40a476d3a4d881398892567423b932
msquaredds <alex.melesko@msquaredds.com>
2025-02-20 14:40:06 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import streamlit as st
 
+
 def _set_state():
     st.session_state.test = True
 
 
 def main():
     st.button("Test", on_click=_set_state)
-    if st.session_state.test:
+    if if 'test' in st.session_state and st.session_state.test:
         st.write("Test is True")
 
 
